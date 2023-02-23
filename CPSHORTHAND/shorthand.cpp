11ed18a22c97eb814c446884e204bbc4f8a663ce
Prateekk02 <99165065+Prateekk02@users.noreply.github.com>
2023-02-24 01:44:46 +0530

    Local Machine.

---
+++ CPSHORTHAND/shorthand.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define fr_n(i,a,b) for(int i=a; i<=b;i++)
+#define fr(i,a,b) for(int i=a; i<n;i++)
+#define sq(a) (a)*(a)
+#define fr_v(it,a) for(auto it=a.begin(); it!=a.end(); it++)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    
+    return 0;
+}
